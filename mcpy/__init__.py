e258ddeeccf1a746b463f28abaafb606a27085d9
Juha Jeronen <juha.m.jeronen@gmail.com>
2020-09-26 00:01:46 +0300

    update setup.py
    Next version should be 2.1.0 (following SemVer), since backward compatible.

--- mcpy/__init__.py
+++ mcpy/__init__.py
@@ -1,4 +1,5 @@
-from .unparse import unparse
-from .utilities import gensym
+from .unparse import unparse  # noqa: F401
+from .utilities import ast_aware_repr, gensym  # noqa: F401
+from .visitors import MacroExpansionError  # noqa: F401
 
-__version__ = '3.0.0'
+__version__ = '2.1.0'
--- setup.py
+++ setup.py
@@ -1,27 +1,45 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-from distutils.core import setup
+import os
+from setuptools import setup
+
+def read(*relpath, **kwargs):
+    with open(os.path.join(os.path.dirname(__file__), *relpath),
+              encoding=kwargs.get('encoding', 'utf8')) as fh:
+        return fh.read()
 
 setup(
-    name = "mcpy",
-    packages = ["mcpy"],
-    version = "2.0.0",
-    description = "A small and compact Python 3 library to enable syntactic macros at importing time",
-    author = "Salvador de la Puente González",
-    author_email = "salva@unoyunodiez.com",
-    url = "https://github.com/delapuente/mcpy",
-    download_url = "https://github.com/delapuente/mcpy/tarball/2.0.0",
-    keywords = ["macros", "import", "importer", "github"],
-    classifiers = [
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3 :: Only",
+    name="mcpy",
+    version="2.1.0",
+    packages=["mcpy"],
+    provides=["mcpy"],
+    keywords=["macros", "syntactic-macros", "import", "importer", "github"],
+    python_requires=">=3.6",
+    author="Salvador de la Puente González and Juha Jeronen",
+    author_email="salva@unoyunodiez.com",
+    url="https://github.com/delapuente/mcpy",
+    description="A small and compact Python 3 library to enable syntactic macros at import time",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    platforms=["Any"],
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Other Environment",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: Code Generators",
         "Topic :: Utilities"
-    ]
+    ],
+    zip_safe=False  # macros are not zip safe, because the zip importer fails to find sources.
 )
