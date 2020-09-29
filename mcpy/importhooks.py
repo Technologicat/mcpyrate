018250af9f964430e85872cf3c32130e7cbfd529
Juha Jeronen <juha.m.jeronen@gmail.com>
2020-09-30 02:12:00 +0300

    docstring/linebreak

--- mcpy/importhooks.py
+++ mcpy/importhooks.py
@@ -10,13 +10,13 @@ from .markers import get_markers
 def nop(*args, **kw): pass
 
 def source_to_xcode(self, data, path, *, _optimize=-1):
-    '''Intercepts the source to code transformation and expand the macros
-    before compiling to actual code.'''
+    '''Expand macros before compiling.
+
+    Intercepts the source to bytecode transformation.'''
     tree = ast.parse(data)
     module_macro_bindings = find_macros(tree)
     expansion = expand_macros(tree, bindings=module_macro_bindings, filename=path)
     remaining_markers = get_markers(expansion)
     if remaining_markers:
         raise MacroExpansionError("{path}: AST markers remaining after expansion: {remaining_markers}")
-    return compile(expansion, path, 'exec', dont_inherit=True,
-                   optimize=_optimize)
+    return compile(expansion, path, 'exec', dont_inherit=True, optimize=_optimize)
