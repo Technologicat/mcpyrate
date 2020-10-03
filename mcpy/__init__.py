85431b7f15cb6fe8e2642284e12a2cda5d3a0b6f
Juha Jeronen <juha.m.jeronen@gmail.com>
2020-10-03 17:13:30 +0300

    publish @parametricmacro at top level

--- mcpy/__init__.py
+++ mcpy/__init__.py
@@ -1,6 +1,6 @@
 from .astdumper import dump  # noqa: F401
 from .core import MacroExpansionError  # noqa: F401
-from .expander import namemacro  # noqa: F401
+from .expander import namemacro, parametricmacro  # noqa: F401
 from .unparser import unparse  # noqa: F401
 from .utilities import gensym  # noqa: F401
 
