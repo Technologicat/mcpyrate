4edc0d6e05e2a4febc17424038c87741527af5cd
Juha Jeronen <juha.m.jeronen@gmail.com>
2020-10-10 17:57:43 +0300

    import dialects.Dialect by default

--- mcpy/__init__.py
+++ mcpy/__init__.py
@@ -1,5 +1,6 @@
 from .astdumper import dump  # noqa: F401
 from .core import MacroExpansionError  # noqa: F401
+from .dialects import Dialect  # noqa: F401
 from .expander import namemacro, parametricmacro  # noqa: F401
 from .unparser import unparse  # noqa: F401
 from .utils import gensym  # noqa: F401
