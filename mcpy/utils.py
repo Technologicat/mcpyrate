b7a40dac59dea2968e657c0a352d79a8b94565f3
Juha Jeronen <juha.m.jeronen@gmail.com>
2020-10-11 14:31:39 +0300

    Add `mcpy.utils.rename` to rename identifiers in ASTs

--- mcpy/utils.py
+++ mcpy/utils.py
@@ -4,10 +4,14 @@
 __all__ = ['gensym', 'flatten_suite',
            'format_location', 'format_macrofunction',
            'NestingLevelTracker',
-           'NodeVisitorListMixin', 'NodeTransformerListMixin']
+           'NodeVisitorListMixin', 'NodeTransformerListMixin',
+           'rename']
 
+import ast
 from contextlib import contextmanager
 import uuid
+
+from . import walker
 
 
 _previous_gensyms = set()
@@ -136,3 +140,56 @@ class NodeTransformerListMixin:
         if isinstance(tree, list):
             return flatten_suite(self.visit(elt) for elt in tree)
         return super().visit(tree)
+
+
+def rename(from_, to, tree):
+    """Rename all occurrences of a name in `tree`.
+
+    We look in all places in the AST that hold name-like things.
+
+    Currently: identifiers (names), attribute names, function and class names,
+    function parameters, arguments passed by name, name and asname in imports,
+    and the as-part of an exception handler.
+
+    Some constructs such as `For` and `With` use `Name` nodes for named things,
+    so those are transformed too.
+
+    With this you can do things like::
+
+        from mcpy.quotes import macros, q
+        from mcpy import gensym
+        from mcpy.utils import rename
+
+        tree = q[lambda _: ...]
+        tree = rename("_", gensym(), tree)
+    """
+    class Renamer(walker.Walker):
+        def transform(self, tree):
+            T = type(tree)
+            if T is ast.Name:
+                if tree.id == from_:
+                    tree.id = to
+            # Look for "raw string" in GTS for a full list of the following.
+            # https://greentreesnakes.readthedocs.io/en/latest/nodes.html
+            elif T is ast.Attribute:
+                if tree.attr == from_:
+                    tree.attr = to
+            elif T in (ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef):
+                if tree.name == from_:
+                    tree.name = to
+            elif T is ast.arg:  # function parameter
+                if tree.arg == from_:
+                    tree.arg = to
+            elif T is ast.keyword:  # in function call, argument passed by name
+                if tree.arg == from_:
+                    tree.arg = to
+            elif T is ast.alias:  # in ast.Import, ast.ImportFrom
+                if tree.name == from_:
+                    tree.name = to
+                if tree.asname == from_:
+                    tree.asname = to
+            elif T is ast.ExceptHandler:
+                if tree.name == from_:
+                    tree.name = to
+            return self.generic_visit(tree)
+    return Renamer().visit(tree)
