be54d1021193110829bef3398acfdc66a363b6e3
Juha Jeronen <juha.m.jeronen@gmail.com>
2020-11-13 17:14:53 +0200

    PEP8

--- mcpyrate/bunch.py
+++ mcpyrate/bunch.py
@@ -43,27 +43,44 @@ class Bunch:
         del self._data[name]
 
     # Container, Iterable, Sized
-    def __contains__(self, name): return self._data.__contains__(name)
-    def __iter__(self): return self._data.__iter__()
-    def __len__(self): return len(self._data)
+    def __contains__(self, name):
+        return self._data.__contains__(name)
+    def __iter__(self):
+        return self._data.__iter__()
+    def __len__(self):
+        return len(self._data)
 
     # Mapping
-    def __getitem__(self, name): return self._data.__getitem__(name)
-    def __setitem__(self, name, value): return self._data.__setitem__(name, value)
-    def __delitem__(self, name): return self._data.__delitem__(name)
-    def keys(self): return self._data.keys()
-    def items(self): return self._data.items()
-    def values(self): return self._data.values()
-    def get(self, name, default=None): return self[name] if name in self else default
-    def __eq__(self, other): return other == self._data
-    def __ne__(self, other): return other != self._data
+    def __getitem__(self, name):
+        return self._data.__getitem__(name)
+    def __setitem__(self, name, value):
+        return self._data.__setitem__(name, value)
+    def __delitem__(self, name):
+        return self._data.__delitem__(name)
+    def keys(self):
+        return self._data.keys()
+    def items(self):
+        return self._data.items()
+    def values(self):
+        return self._data.values()
+    def get(self, name, default=None):
+        return self[name] if name in self else default
+    def __eq__(self, other):
+        return other == self._data
+    def __ne__(self, other):
+        return other != self._data
 
     # MutableMapping
-    def pop(self, name, *default): return self._data.pop(name, *default)
-    def popitem(self): return self._data.popitem()
-    def clear(self): return self._data.clear()
-    def update(self, **bindings): self._data.update(**bindings)
-    def setdefault(self, name, *default): return self._data.setdefault(name, *default)
+    def pop(self, name, *default):
+        return self._data.pop(name, *default)
+    def popitem(self):
+        return self._data.popitem()
+    def clear(self):
+        return self._data.clear()
+    def update(self, **bindings):
+        self._data.update(**bindings)
+    def setdefault(self, name, *default):
+        return self._data.setdefault(name, *default)
 
     def __repr__(self):  # pragma: no cover
         bindings = [f"{name:s}={repr(value)}" for name, value in self._data.items()]
