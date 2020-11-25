d60b51a99485e0bc1cd0c6766affa5fc030f1b65
Juha Jeronen <juha.m.jeronen@gmail.com>
2020-11-26 00:17:12 +0200

    improve example

--- demo/anaphoric_if_revisited/let.py
+++ demo/anaphoric_if_revisited/let.py
@@ -1,53 +1,48 @@
 # -*- coding: utf-8; -*-
 """Alternative file layout for the let example. Macro definitions.
 
-This file is still multi-phase compiled, because `letseq` needs to refer to
-`let` hygienically, and we want to define both macros in the same module.
+We also make another change to the first example, to demonstrate a useful
+technique: hygienic macro recursion. Now we don't need multi-phase compilation.
 """
 
 __all__ = ["let", "letseq"]
 
-from mcpyrate.multiphase import macros, phase
+from mcpyrate.quotes import macros, q, a, t  # noqa: F811, F401
 
-with phase[1]:
-    from mcpyrate.quotes import macros, q, n, a, t, h  # noqa: F811, F401
+from ast import arg
 
-    from ast import arg
-
-    from mcpyrate import parametricmacro
-
-    @parametricmacro
-    def let(tree, *, args, syntax, **kw):
-        """[syntax, expr] Bind expression-local variables.
-
-        Usage::
-
-            let[[k0, v0], ...][expr]
-
-        `let` expands into a `lambda`::
-
-            let[[x, 1], [y, 2]][print(x, y)]
-            # --> (lambda x, y: print(x, y))(1, 2)
-        """
-        if syntax != "expr":
-            raise SyntaxError("`let` is an expr macro only")  # pragma: no cover
-        if not args:
-            raise SyntaxError("expected at least one binding")  # pragma: no cover
-
-        # args: `list` of `ast.List`. Each sublist is [ast.Name, expr].
-        names = [k.id for k, _ in (a.elts for a in args)]
-        values = [v for _, v in (a.elts for a in args)]
-        if len(set(names)) < len(names):
-            raise SyntaxError("binding names must be unique in the same `let`")  # pragma: no cover
-
-        lam = q[lambda: a[tree]]
-        lam.args.args = [arg(arg=x) for x in names]
-        return q[a[lam](a[values])]
+from mcpyrate import parametricmacro
+from mcpyrate.quotes import capture_as_macro
 
 
-from __self__ import macros, let  # noqa: F811, F401
+@parametricmacro
+def let(tree, *, args, syntax, **kw):
+    """[syntax, expr] Bind expression-local variables.
 
-from mcpyrate.utils import extract_bindings
+    Usage::
+
+        let[[k0, v0], ...][expr]
+
+    `let` expands into a `lambda`::
+
+        let[[x, 1], [y, 2]][print(x, y)]
+        # --> (lambda x, y: print(x, y))(1, 2)
+    """
+    if syntax != "expr":
+        raise SyntaxError("`let` is an expr macro only")  # pragma: no cover
+    if not args:
+        raise SyntaxError("expected at least one binding")  # pragma: no cover
+
+    # args: `list` of `ast.List`. Each sublist is [ast.Name, expr].
+    names = [k.id for k, _ in (a.elts for a in args)]
+    values = [v for _, v in (a.elts for a in args)]
+    if len(set(names)) < len(names):
+        raise SyntaxError("binding names must be unique in the same `let`")  # pragma: no cover
+
+    lam = q[lambda: a[tree]]
+    lam.args.args = [arg(arg=x) for x in names]
+    return q[a[lam](a[values])]
+
 
 @parametricmacro
 def letseq(tree, *, args, syntax, expander, **kw):
@@ -71,7 +66,9 @@ def letseq(tree, *, args, syntax, expander, **kw):
     if not args:
         return tree
     first, *rest = args
-    macro_bindings = extract_bindings(expander.bindings, letseq)
-    letseq_macroname = list(macro_bindings.keys())[0]
-    body = q[n[letseq_macroname][t[rest]][a[tree]]]
-    return q[h[let][a[first]][a[body]]]
+    body = q[a[our_letseq][t[rest]][a[tree]]]
+    return q[a[our_let][a[first]][a[body]]]
+
+
+our_let = capture_as_macro(let)
+our_letseq = capture_as_macro(letseq)
