e3b0a0f628fb323add74053b8e3ef9c44a054b96
Juha Jeronen <juha.m.jeronen@gmail.com>
2020-12-03 01:45:25 +0200

    clean implementation in preparation of publishing clearcaches util

--- clearcaches.py
+++ clearcaches.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8; -*-
-"""Clear (delete) .pyc caches inside given directory, recursively.
+"""Delete Python bytecode (`.pyc`) caches inside given directory, recursively.
 
-This makes it easier to test `mcpyrate`, because the expander only runs when
-the `.pyc` cache for the module being imported is out of date or does not
-exist. Python's import system will then write the `.pyc` cache for that module.
+This removes the `__pycache__` directories, too.
+
+This facilitates testing `mcpyrate` and programs that use it, because the
+expander only runs when the `.pyc` cache for the module being imported is out
+of date or does not exist. For any given module, once the expander is done,
+Python's import system will then write the `.pyc` cache for that module.
 """
+
+__all__ = ["getpycachedirs", "deletepycachedirs"]
 
 import argparse
 import os
@@ -12,14 +17,13 @@ import textwrap
 
 __version__ = "3.0.0"
 
-def getpycachedirs(path):
-    paths = []
-    for root, dirs, files in os.walk(path):
-        if "__pycache__" in dirs:
-            paths.append(os.path.join(root, "__pycache__"))
-    return paths
 
-def rmrf(path):
+def _delete_directory_recursively(path):
+    """Delete a directory recursively, like 'rm -rf' in the shell.
+
+    Ignores `FileNotFoundError`, but other errors raise. If an error occurs,
+    some files and directories may already have been deleted.
+    """
     for root, dirs, files in os.walk(path, topdown=False, followlinks=False):
         for x in files:
             try:
@@ -32,37 +36,58 @@ def rmrf(path):
     except FileNotFoundError:
         pass
 
-def clean(path, dry_run=False):
+
+def getpycachedirs(path):
+    """Return a list of all `__pycache__` directories under `path` (str).
+
+    Each of the entries starts with `path`.
+    """
     if not os.path.isdir(path):
         raise OSError(f"No such directory: '{path}'")
 
-    pycachedirs = getpycachedirs(path)
-    if dry_run:
-        for x in pycachedirs:
-            print(x)
-        return
-    else:
-        for x in pycachedirs:
-            rmrf(x)
+    paths = []
+    for root, dirs, files in os.walk(path):
+        if "__pycache__" in dirs:
+            paths.append(os.path.join(root, "__pycache__"))
+    return paths
+
+
+def deletepycachedirs(path):
+    """Delete all `__pycache__` directories under `path` (str).
+
+    Ignores `FileNotFoundError`, but other errors raise. If an error occurs,
+    some `.pyc` cache files and their directories may already have been deleted.
+    """
+    for x in getpycachedirs(path):
+        _delete_directory_recursively(x)
+
 
 def main():
     parser = argparse.ArgumentParser(description=textwrap.dedent(
         """
-        Clear (delete) .pyc caches inside given directory, recursively.
+        Delete Python bytecode (`.pyc`) caches inside given directory, recursively.
 
-        This makes it easier to test `mcpyrate`, because the expander only runs when
-        the `.pyc` cache for the module being imported is out of date or does not
-        exist. Python's import system will then write the `.pyc` cache for that module.
+        This removes the `__pycache__` directories, too.
+
+        This facilitates testing `mcpyrate` and programs that use it, because the
+        expander only runs when the `.pyc` cache for the module being imported is out
+        of date or does not exist. For any given module, once the expander is done,
+        Python's import system will then write the `.pyc` cache for that module.
         """),
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-v', '--version', action='version', version=('%(prog)s ' + __version__))
-    parser.add_argument(dest="path", type=str, metavar="path", help="directory path (can be relative)")
+    parser.add_argument(dest="path", type=str, metavar="path", help="directory path to start in (absolute or relative)")
     parser.add_argument("-n", "--dry-run", dest="dry_run", action="store_true", default=False,
-                        help="only show the .pyc cache directory names; don't clear them.")
+                        help="scan for and print the .pyc cache directory paths, and exit.")
 
     opts = parser.parse_args()
 
-    clean(opts.path, opts.dry_run)
+    if not opts.dry_run:
+        deletepycachedirs(opts.path)
+    else:
+        for x in getpycachedirs(opts.path):
+            print(x)
+
 
 if __name__ == "__main__":
     main()
