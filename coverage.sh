236050364e978d6cec3c198f8cef16a061004b32
Juha Jeronen <juha.m.jeronen@gmail.com>
2021-05-09 02:18:57 +0300

    document difference between the two coverage scripts

--- coverage.sh
+++ coverage.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# This measures the coverage for a single macro-enabled script or module.
 coverage run --source=. -m mcpyrate.repl.macropython $1
 coverage html
--- measure_coverage.sh
+++ measure_coverage.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This measures the coverage for all automated tests.
 
 # https://github.com/nedbat/coveragepy
 # pip install coverage
