d3411c83d87cd1e80a52c23a6cf005ad9f359ec7
Juha Jeronen <juha.m.jeronen@gmail.com>
2024-09-26 16:23:55 +0300

    metadata: Python 3.11 and 3.12 now supported

--- setup.py
+++ setup.py
@@ -67,6 +67,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development :: Libraries",
